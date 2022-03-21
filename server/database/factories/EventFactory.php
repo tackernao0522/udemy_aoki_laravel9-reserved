10249570ea2ada271b4989d5227c8d58fd6609f6
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-21 13:51:38 +0900

    49 ダミーデータの生成(factory, faker)

--- server/database/factories/EventFactory.php
+++ server/database/factories/EventFactory.php
@@ -16,8 +16,15 @@ class EventFactory extends Factory
      */
     public function definition()
     {
+        $dummyDate = $this->faker->dateTimeThisMonth;
+
         return [
-            //
+            'name' => $this->faker->name,
+            'information' => $this->faker->realText,
+            'max_people' => $this->faker->numberBetween(1, 20),
+            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
+            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'is_visible' => $this->faker->boolean,
         ];
     }
 }
--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
         ]);
+
+        Event::factory(100)->create();
     }
 }
