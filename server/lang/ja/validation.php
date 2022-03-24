87f8773870dae98e62ee60e891c9bccbc4d0e480
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-24 16:47:42 +0900

    58 バリデーション

--- server/lang/ja/validation.php
+++ server/lang/ja/validation.php
@@ -154,9 +154,15 @@ return [
     */
 
   'attributes' => [
-    'name' => '名前',
     'email' => 'メールアドレス',
     'password' => 'パスワード',
+    'name' => '名前',
+    'event_name' => 'イベント名',
+    'information' => 'イベント詳細',
+    'event_date' => 'イベントの日付',
+    'start_time' => '開始時間',
+    'end_time' => '終了時間',
+    'max_people' => '定員',
   ],
 
 ];
