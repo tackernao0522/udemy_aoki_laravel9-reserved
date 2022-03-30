9a8a3b6bb23a6e05608d5b83f94a92b3d972ca44
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-30 19:55:25 +0900

    79 予約情報のレイアウト調整

--- server/resources/views/manager/events/show.blade.php
+++ server/resources/views/manager/events/show.blade.php
@@ -69,13 +69,31 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="max-w-2xl py-4 mx-auto">
                     @if (!$users->isEmpty())
-                        予約情報
-                        @foreach ($reservations as $reservation)
-                            @if (is_null($reservation['canceled_date']))
-                                {{ $reservation['name'] }}
-                                {{ $reservation['number_of_people'] }}
-                            @endif
-                        @endforeach
+                        <div class="text-center py-2">
+                            予約状況
+                        </div>
+                        <table class="table-auto w-full text-left whitespace-no-wrap">
+                            <thead>
+                                <tr>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        予約者名</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        予約人数</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($reservations as $reservation)
+                                    @if (is_null($reservation['canceled_date']))
+                                        <tr>
+                                            <td class="px-4 py-3">{{ $reservation['name'] }}</td>
+                                            <td class="px-4 py-3">{{ $reservation['number_of_people'] }}</td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
                     @endif
                 </div>
             </div>
