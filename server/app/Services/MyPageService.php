f8c8fa48415ce13cc24b69a35e44e38af45f33e3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-10 16:38:20 +0900

    101 マイページの準備、コントローラ

---
+++ server/app/Services/MyPageService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class MyPageService
+{
+  public static function reservedEvent($events, $string)
+  {
+    $reservedEvents = [];
+    if($string === 'fromToday') {
+
+    } elseif ($string === 'past') {
+
+    }
+
+    return $reservedEvents;
+  }
+}
