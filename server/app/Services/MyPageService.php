6c06be5ca3baf89ea0fe5812577d12b73d04d2db
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-10 17:26:22 +0900

    102 予約したイベントの取得(今日を含む未来と過去それぞれ)

--- server/app/Services/MyPageService.php
+++ server/app/Services/MyPageService.php
@@ -2,15 +2,41 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+
 class MyPageService
 {
   public static function reservedEvent($events, $string)
   {
     $reservedEvents = [];
-    if($string === 'fromToday') {
-
+    if ($string === 'fromToday') {
+      foreach ($events->sortBy('start_date') as $event) {
+        if (
+          is_null($event->pivot->canceled_date) && $event->start_date >= Carbon::now()
+          ->format('Y-m-d 00:00:00')
+        ) {
+          $eventInfo = [
+            'name' => $event->name,
+            'start_date' => $event_date,
+            'number_of_people' => $event->pivot->numbar_of_people,
+          ];
+          array_push($reservedEvents, $eventInfo);
+        }
+      }
     } elseif ($string === 'past') {
-
+      foreach ($events->sortByDesc('start_date') as $event) {
+        if (is_null($event->pivot->canceled_date) && $event->start_date < Carbon::now()
+          ->format('Y-m-d 00:00:00')
+        ) {
+          $eventInfo = [
+            'name' => $event->name,
+            'start_date' => $event->start_date,
+            'end_date' => $event->end_date,
+            'number_of_people' => $event->pivot->number_of_people,
+          ];
+          array_push($reservedEvents, $eventInfo);
+        }
+      }
     }
 
     return $reservedEvents;
