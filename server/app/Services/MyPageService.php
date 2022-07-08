f21f40f11f6e2c03bcd922fd7fa8797f93a3dc28
tackernao0522 <53789796+tackernao0522@users.noreply.github.com>
2022-07-08 18:36:40 +0900

    Update MyPageService.php

--- server/app/Services/MyPageService.php
+++ server/app/Services/MyPageService.php
@@ -12,13 +12,14 @@ class MyPageService
     if ($string === 'fromToday') {
       foreach ($events->sortBy('start_date') as $event) {
         if (
-          is_null($event->pivot->canceled_date) && $event->start_date >= Carbon::now()
-          ->format('Y-m-d 00:00:00')
+          is_null($event->pivot->canceled_date) &&
+          $event->start_date >= Carbon::now()->format('Y-m-d 00:00:00')
         ) {
           $eventInfo = [
             'id' => $event->id,
             'name' => $event->name,
-            'start_date' => $event_date,
+            'start_date' => $event->start_date,
+            'end_date' => $event->end_date,
             'number_of_people' => $event->pivot->numbar_of_people,
           ];
           array_push($reservedEvents, $eventInfo);
